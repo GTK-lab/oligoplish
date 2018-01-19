c9b003e35c9eb8df007a7947b3bab751da0d3773
Greg Tucker-Kellogg <dbsgtk@gmail.com>
2018-01-19 10:03:03 +0800

    updated to use a relative projectdir

--- scripts/clean_oligoarray_results.R
+++ scripts/clean_oligoarray_results.R
@@ -1,13 +1,43 @@
-# This has to be run with the "ranges" object still existing from the previous script.
+## This generates the oligopaints bed files without the added primers
 
 library(here)
 library(tidyverse)
-oligos <- read_tsv(here(sprintf('results/%s/oligos.txt',project)),
-              col_names <- c("name","pos","len",
+library(rtracklayer)
+library(GenomicRanges)
+library(GenomicFeatures)
+library(Biostrings)
+project <- "controls"
+
+
+projectdir <- project
+
+target_ranges <- rtracklayer::import(sprintf("%s/targets.bed",projectdir))
+oa_ranges <- rtracklayer::import(sprintf("%s/oa_targets.bed",projectdir))
+target_ranges
+oa_ranges
+oligos <- read_tsv(sprintf('%s/oligos.txt',projectdir),
+                   col_types = cols(),
+                   col_names <- c("name","pos","len",
                              "deltaG","deltaH","deltaS","Tm","targets","seq")) %>%
     filter(!str_detect(targets,";"))
 
-oligo_table <- inner_join(as_data_frame(as.data.frame(ranges)),oligos,by="name")
-oligo_bed <- oligo_table %>% transmute(chr=seqnames,start=start+pos-1,end=start+len,name=name)
+oligo_table <- inner_join(as_data_frame(as.data.frame(oa_ranges)),oligos,by="name") %>%
+    mutate(gene=str_extract(name,"^[A-Za-z0-9]+")) %>%
+    group_by(gene) %>%
+    mutate(name=sprintf("%s_o%02d",gene,row_number()),
+           start=start+pos-1,
+           end=start+len)  %>% ungroup()
 
-write_tsv(oligo_bed,here(sprintf("results/%s/oligos.bed",project)),col_names=FALSE)
+rtracklayer::export(oligo_table,sprintf("%s/oligos.bed",projectdir))
+
+dna <- Biostrings::DNAStringSet(oligo_table$seq)
+names(dna) <- oligo_table$name
+writeXStringSet(dna,sprintf("%s/oligopaints.fa",projectdir))
+
+
+for (g in unique(oligo_table$gene)) {
+    print(g)
+    filter(oligo_table,gene==g) %>%
+        dplyr::select(seqnames,start,end,name=seq,score=Tm) %>%
+        rtracklayer::export(sprintf("%s/%s_oligopaints.bed",projectdir,g))
+    }
--- scripts/prepOligoArray.R
+++ scripts/prepOligoArray.R
@@ -24,28 +24,31 @@ project <- args$project
 cat("ready to go\n")
 f <- file("stdin")
 syms <- readLines(f)
-cat(sprintf("generating sequences for %d gene symbols\n",length(syms)))
 
-                                        # This file is all hg38 with refseq ids, downloaded from ensembl
+## This file is all hg38 with refseq ids, downloaded from ensembl
 hg38_tss_fn <- 'data/hg38-tss.tsv'
 
 ## Don't change below this line
 
-                                        # read in ENSEMBL hg38 transcript tss locations with refseq mrnas
+## read in ENSEMBL hg38 transcript tss locations with refseq mrnas
 txdb <- read_tsv(here(hg38_tss_fn),skip=1,col_types=cols(),
                  col_names=c('gene_id','ts_id','tss','geneid','refseq','symbol','seqname','strand')) %>%
     filter(!is.na(geneid),!str_detect(seqname,"[_.]")) %>%
     mutate(strand=fct_recode(factor(strand),"+"="1","-"="-1"),
+           seqname=sprintf('chr%s',seqname),
            start=tss,end=tss)
 
-for (s in syms) {
-    if (!(s %in% txdb$symbol)) {
-        cat(sprintf("Symbol '%s' not found.. skipping\n",s))
-    }
+if (any(syms %in% txdb$symbol)) {
+    cat(sprintf("NOT FOUND: '%s'\n",paste(syms[!(syms %in% txdb$symbol)],sep=",")))
 }
-outdir <- sprintf("results/%s",project)
+syms <- syms[syms %in% txdb$symbol]
+cat(sprintf("WORKING ON: '%s'\n",paste(syms,sep=",")))
+
+outdir <- sprintf("%s",project)
 cat(sprintf("results stored in %s\n",outdir))
 dir.create(outdir,showWarnings = FALSE,recursive = TRUE)
+
+writeLines(syms,sprintf("%s/symbols.txt",outdir))
 
 tss <- dplyr::filter(txdb,symbol %in% syms) %>%
     as("GRanges") %>% promoters(upstream,downstream)
@@ -56,7 +59,7 @@ target_ranges$name <- rownames(target_ranges)
 rtracklayer::export(target_ranges,sprintf("%s/targets.bed",outdir))
 
 oa_ranges <- rowwise(target_ranges) %>%
-    do(tibble(chr=sprintf("chr%s",.$seqnames),
+    do(tibble(seqnames=.$seqnames,
               start=seq(from=.$start,to=.$end,by=950),
               end=.$end,
               name=sprintf(fmt="%s_%d",.$name,seq_along(start)))) %>%
@@ -67,6 +70,3 @@ names(seqs) <- oa_ranges$name
 
 rtracklayer::export(oa_ranges,sprintf("%s/oa_targets.bed",outdir))
 writeXStringSet(seqs,sprintf("%s/oa_targets.fa",outdir))
-## write_tsv(as(oa_ranges,"data.frame"),sprintf("%s/targets.tsv",outdir))
-## write_tsv(as(oa_ranges,"data.frame") %>% dplyr::select(seqnames,start,end,name),
-##           sprintf("%s/targets.bed",outdir),col_names=FALSE)
